f19aac02697dd3a74db8baa145b236df3676c046
suzie4831 <suzie4831@naver.com>
2019-07-30 18:47:22 +0900

    first commit

---
+++ blogapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Blog
+from django.utils import timezone
+import requests
+# Create your views here.
+
+def home(request):
+    blogs=Blog.objects
+    return render(request, 'home.html', {'blogs':blogs})
+
+def detail(request,blog_id):
+    blog_object=get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'detail.html',{'blog':blog_object})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    blog=Blog()
+    blog.title=request.GET['title']
+    blog.body=request.GET['body']
+    blog.pub_date=timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/'+str(blog.id))
+
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
